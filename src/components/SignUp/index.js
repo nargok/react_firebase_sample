685de64b287b0ab60cb02c8a869b512e9333c6aa
nargok <nargok@gmail.com>
2019-02-18 08:51:25 +0900

    input fields in SignUp component

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose;
 
+import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
 const SignUpPage = () => (
@@ -30,13 +32,51 @@ class SignUpForm extends Component {
   }
 
   onChange = event => {
-
+    this.setState({ [event.target.name]: event.target.value })
   }
 
   render() {
+    const {
+      username,
+      email,
+      passwordOne,
+      passwordTwo,
+      error
+    } = this.state;
+
     return (
       <form onSubmit={this.onSubmit}>
+        <input
+          name="username"
+          value={username}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Full name"
+        />
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="email"
+          placeholder="Email Address"
+        />
+        <input
+          name="passwordOne"
+          value={passwordOne}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          name="passwordTwo"
+          value={passwordTwo}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <button type="submit">Sign Up</button>
 
+        {error && <p>{error.message}</p>}
       </form>
     );
   }
@@ -47,6 +87,11 @@ const SignUpLink = () => (
     Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
   </p>
 );
+
+const SignUpForm = compose(
+  withRouter,
+  withFirebase,
+)(SignUpFormBase);
 
 export default SignUpPage;
 
